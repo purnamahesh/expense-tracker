ed6a31f5dacac56fd6bbb1729b32a19abf1d1375
mmv6113 <pimmandi66@massmutual.com>
2025-10-21 20:37:29 +0530

    add expense

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,39 @@
-use std::env::args;
+use std::{env::args, fs::File, io::{self, Error, ErrorKind}, process::exit};
 
 pub mod utils;
 
-use crate::utils::get_and_prcess_input;
+use crate::utils::{expense::Expense};
 
 fn main() {
 
     let args:Vec<String> = args().collect();
 
-    for arg in args {
-        println!("{}", arg)
+    if args.len() == 1 {
+        println!("HELP")
+    } else if args.len() == 2 {
+        if args[1] == "help" {
+            println!("HELP")
+        }
+        else if args[1] == "list" {
+            
+        }
+        else if args[1] == "total" {
+            
+        }
+    } else {
+        if args.len() % 2 != 0 {
+            println!("HELP");
+            exit(1)
+        }
+        else if args[1] == "add" {
+            let new_expense = Expense::new(&args);
+            dbg!(new_expense);
+        }
+        else if args[1] == "filter" {
+            
+        } else {
+            println!("HELP")
+        }
     }
 
     // get_and_prcess_input();
--- src/utils/expense.rs
+++ src/utils/expense.rs
@@ -1,19 +1,63 @@
-use crate::utils::category::Category;
-use crate::utils::tags::Tag;
-use crate::utils::stdin_utils;
+use std::{process::exit, vec};
 
-pub struct Expense {
+#[derive(Debug)]
+pub struct Expense<'a> {
     amount: f64,
-    description: String,
-    category: Category,
-    tags: Vec<Tag>
+    description: Option<&'a str>,
+    category: Option<&'a str>,
+    tags: Vec<&'a str>
+    // ,datetime TODO
 }
 
-impl Expense {
-    fn get_expense(&self) {
-        let amount = stdin_utils::read_f64("Enter amount:");
-        let desc = stdin_utils::read_sting("Enter Description:");
-        // let amount = stdin_utils::get_index("Choose Category:"); //TODO
-        // let tags = stdin_utils::add_tags("Enter amount:");
+impl<'a> Expense<'a> {
+    pub fn new(args: &'a Vec<String>) -> Expense<'a> {
+        let (amount, description, category, tags) = Self::parse_args(args);
+        Expense { amount: amount, description: description, category: category, tags: tags }
+    }
+
+    fn parse_args(args: &'a Vec<String>) -> (f64, Option<&str>, Option<&str>, Vec<&str>){
+        let mut description: Option<&str> = None;
+        let mut amount: f64 = 0.0;
+        let mut category: Option<&str> = None;
+        let mut tags: Vec<&'a str> = vec![];
+
+        for i in (2..args.len()) {
+            if i % 2 == 0 {
+                match args[i].as_str() {
+                    "--description" => {
+                        description = Some(&args[i+1]);
+                    },
+                    "--amount" => {
+                        amount = args[i+1].trim().parse().expect("Error whiel parsing");
+                    },
+                    "--category" => {
+                        category = Some(&args[i+1]);
+                    },
+                    "--tags" => {
+                        tags = args[i+1].split(',').collect();
+                    },
+                    _ => {
+                        println!("Invalid argument {}", args[i]);
+                        exit(1)
+                    }
+                }
+            }
+        }
+
+        Self::validate_expense(amount, category);
+
+        (amount, description, category, tags)
+    }
+
+    fn validate_expense(amount: f64, category: Option<&'a str>) -> bool {
+        if amount == 0.0 {
+            println!("Amount is required!") ;
+            return false;
+        }
+        else if category.is_none() || category == Some(String::from("").as_str()) {
+            println!("category is required!");
+            return false;
+        }
+        return true;
     }
 }
