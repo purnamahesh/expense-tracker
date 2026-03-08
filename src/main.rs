2523169a2b80b0d23dda0247079287523d8b72f3
purnamahesh <poorna.sd9@gmail.com>
2026-03-08 17:29:08 +0530

    feat: features

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[cfg(all(feature = "full", feature = "lite"))]
+compile_error!("Features `full` and `lite` are mutually exclusive. Please enable only one.");
+
 use clap::Parser;
 use std::error::Error;
 
